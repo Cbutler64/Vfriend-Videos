6db07d402afb3682599b5164202858afe1880772
Chad Butler <you@example.com>
2018-10-16 18:29:51 -0400

    final tweaks

--- server.js
+++ server.js
@@ -13,14 +13,13 @@ app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds131963.mlab.com:31963/heroku_mpd7kw39" || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds131963.mlab.com:31963/heroku_mpd7kw39" || "mongodb://localhost/vFriendReactUsers";
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-//mongoose.connect(MONGODB_URI);
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-// Connect to the Mongo DB
+
 // mongoose.connect(
-//   process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds131963.mlab.com:31963/heroku_mpd7kw39" ||"mongodb://localhost/vFriendReactUsers"
+//   process.env.MONGODB_URI || "mongodb://localhost/vFriendReactUsers"
 // );
 
 // Start the API server
